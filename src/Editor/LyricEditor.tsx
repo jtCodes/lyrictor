aa6f1bead7e41001f0bc9888fa3d549f6b41a1a6
jt <jt234wq@gmail.com>
2022-02-18 11:49:30 -0500

    playing audio via howler

--- src/Editor/LyricEditor.tsx
+++ src/Editor/LyricEditor.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useRef } from "react";
 import WaveformData from "waveform-data";
 import { scaleY } from "./utils";
+import { Howl, Howler } from "howler";
 
 export default function LyricEditor() {
+  const url: string =
+    "https://firebasestorage.googleapis.com/v0/b/music-f.appspot.com/o/The%20Gazette%20-%20QUIET%20(%20instrumental).mp3?alt=media&token=1eea4e0d-9539-4cd8-a7d2-cdb94234f0ee";
   const width = 1200;
   const height = 100;
   const canvas = useRef<HTMLCanvasElement | null>(null);
+  const sound = new Howl({
+    src: [url],
+  });
 
   useEffect(() => {
     const canvasCtx = canvas?.current?.getContext("2d");
     const audioCtx = new AudioContext();
 
     generateWaveformDataThrougHttp(audioCtx).then((waveform) => {
+      console.log(waveform);
       console.log(`Waveform has ${waveform.channels} channels`);
       console.log(`Waveform has length ${waveform.length} points`);
 
@@ -20,14 +27,12 @@ export default function LyricEditor() {
   }, []);
 
   async function generateWaveformDataThrougHttp(audioContext: AudioContext) {
-    const response = await fetch(
-      "https://firebasestorage.googleapis.com/v0/b/music-f.appspot.com/o/The%20Gazette%20-%20QUIET%20(%20instrumental).mp3?alt=media&token=1eea4e0d-9539-4cd8-a7d2-cdb94234f0ee"
-    );
+    const response = await fetch(url);
     const buffer = await response.arrayBuffer();
     const options = {
       audio_context: audioContext,
       array_buffer: buffer,
-      scale: 9500,
+      scale: 1000,
     };
     return await new Promise<WaveformData>((resolve, reject) => {
       WaveformData.createFromAudio(options, (err, waveform) => {
@@ -68,6 +73,16 @@ export default function LyricEditor() {
       ctx.fill();
     }
   }
-  
-  return <canvas ref={canvas} width={width} height={height} />;
+
+  return (
+    <canvas
+      ref={canvas}
+      width={width}
+      height={height}
+      onClick={() => {
+        sound.playing() ? sound.pause() : sound.play();
+        console.log(sound.duration());
+      }}
+    />
+  );
 }
