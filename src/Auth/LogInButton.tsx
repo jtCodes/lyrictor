0cfce2f263e1c599a310e753bede7ba9c254b400
jt <jt234wq@gmail.com>
2022-12-19 21:04:13 -0500

    yarn upgrade, migrate demo url to railway

--- src/Auth/LogInButton.tsx
+++ src/Auth/LogInButton.tsx
@@ -2,31 +2,33 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { GoogleLoginButton } from "react-social-login-buttons";
 import { auth, googleProvider } from "../api/firebase";
 
+// TODO: GoogleLoginButton not working after upgrade 
 export default function LogInButton() {
   return (
-    <GoogleLoginButton
-      onClick={() => {
-        signInWithPopup(auth, googleProvider)
-          .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
+    <>LoginButtonTODO</>
+    // <GoogleLoginButton
+    //   onClick={() => {
+    //     signInWithPopup(auth, googleProvider)
+    //       .then((result) => {
+    //         // This gives you a Google Access Token. You can use it to access the Google API.
+    //         const credential = GoogleAuthProvider.credentialFromResult(result);
 
-            if (credential) {
-              console.log(credential);
-            }
-            // ...
-          })
-          .catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-          });
-      }}
-    />
+    //         if (credential) {
+    //           console.log(credential);
+    //         }
+    //         // ...
+    //       })
+    //       .catch((error) => {
+    //         // Handle Errors here.
+    //         const errorCode = error.code;
+    //         const errorMessage = error.message;
+    //         // The email of the user's account used.
+    //         const email = error.email;
+    //         // The AuthCredential type that was used.
+    //         const credential = GoogleAuthProvider.credentialFromError(error);
+    //         // ...
+    //       });
+    //   }}
+    // />
   );
 }
